38e593a5abd35d4516153619e93e4c5c5c4ab34d
Cristian Zangaro <cristian.zangaro@cloud.studio>
2025-04-16 17:21:39 -0300

    Initial commit
    Co-authored-by: Cristian Miguel Zangaro <cristian.zangaro@outlook.com>
    Co-authored-by: DarkSylver <darksylvergs@hotmail.com>

---
+++ PayloadParser.js
@@ -0,0 +1,60 @@
+function parseUplink(device, payload)
+{
+    var parsed = payload.asJsonObject();
+    env.log(parsed);
+
+    var temperatureSensor = device.endpoints.byType(endpointType.temperatureSensor);
+    if (temperatureSensor != null){
+        temperatureSensor.updateTemperatureSensorStatus(parsed.temperature);
+    }
+
+}
+
+function buildDownlink(device, endpoint, command, payload) 
+{ 
+	// This function allows you to convert a command from the platform 
+	// into a payload to be sent to the device.
+	// Learn more at https://wiki.cloud.studio/page/200
+
+	// The parameters in this function are:
+	// - device: object representing the device to which the command will
+	//   be sent. 
+	// - endpoint: endpoint object representing the endpoint to which the 
+	//   command will be sent. May be null if the command is to be sent to 
+	//   the device, and not to an individual endpoint within the device.
+	// - command: object containing the command that needs to be sent. More
+	//   information at https://wiki.cloud.studio/page/1195.
+
+	// This example is written assuming a device that contains a single endpoint, 
+	// of type appliance, that can be turned on, off, and toggled. 
+	// It is assumed that a single byte must be sent in the payload, 
+	// which indicates the type of operation.
+
+/*
+	 payload.port = 25; 	 	 // This device receives commands on LoRaWAN port 25 
+	 payload.buildResult = downlinkBuildResult.ok; 
+
+	 switch (command.type) { 
+	 	 case commandType.onOff: 
+	 	 	 switch (command.onOff.type) { 
+	 	 	 	 case onOffCommandType.turnOn: 
+	 	 	 	 	 payload.setAsBytes([30]); 	 	 // Command ID 30 is "turn on" 
+	 	 	 	 	 break; 
+	 	 	 	 case onOffCommandType.turnOff: 
+	 	 	 	 	 payload.setAsBytes([31]); 	 	 // Command ID 31 is "turn off" 
+	 	 	 	 	 break; 
+	 	 	 	 case onOffCommandType.toggle: 
+	 	 	 	 	 payload.setAsBytes([32]); 	 	 // Command ID 32 is "toggle" 
+	 	 	 	 	 break; 
+	 	 	 	 default: 
+	 	 	 	 	 payload.buildResult = downlinkBuildResult.unsupported; 
+	 	 	 	 	 break; 
+	 	 	 } 
+	 	 	 break; 
+	 	 default: 
+	 	 	 payload.buildResult = downlinkBuildResult.unsupported; 
+	 	 	 break; 
+	 }
+*/
+
+}
